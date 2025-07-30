3e926d228918428d1d7069ddbf245a087574ae16
mthandika <andikaindo931@gmail.com>
2025-07-30 23:14:13 +0800

    Add .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js 1753888452553

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_79561e24._.js",
+  "static/chunks/src_c5581063._.js"
+],
+    source: "dynamic"
+});
